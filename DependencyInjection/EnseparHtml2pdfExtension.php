c5dfb43fce0898a8c09966cc6575a195bb9c2ed2
William <64532799+bm-william@users.noreply.github.com>
2026-04-13 09:43:46 +0200

    Update EnseparHtml2pdfExtension.php

--- DependencyInjection/EnseparHtml2pdfExtension.php
+++ DependencyInjection/EnseparHtml2pdfExtension.php
@@ -4,7 +4,7 @@ namespace Ensepar\Html2pdfBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class EnseparHtml2pdfExtension extends Extension
